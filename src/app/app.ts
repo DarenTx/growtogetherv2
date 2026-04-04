600e41d92046350335e9bac12e4c869de8aea6d1
DarenTx <daren@dahlkids.com>
2026-04-04 16:46:44 -0500

    Made header a link

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   styleUrl: 'app.css',
   template: `
     <div class="app-shell">
       <header class="app-header">
-        <img src="icons/icon-96x96.png" alt="Grow Together logo" width="40" height="40" />
-        <span class="app-title">Grow Together</span>
+        <a routerLink="/dashboard" class="header-link">
+          <img src="icons/icon-96x96.png" alt="Grow Together logo" width="40" height="40" />
+          <span class="app-title">Grow Together</span>
+        </a>
       </header>
       <main class="app-main">
         <router-outlet />
